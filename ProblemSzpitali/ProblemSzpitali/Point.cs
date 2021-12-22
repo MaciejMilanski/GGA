62b3fe7bde4f953ef54440f5945d8a3d315d11d1
MaciejMilanski <maciej.milanski@gmail.com>
2021-12-22 21:05:06 +0100

    k-centrum problem

---
+++ ProblemSzpitali/ProblemSzpitali/Point.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSzpitali
+{
+    public class Point
+    {
+        public Point() 
+        {
+            NeighboursDistances = new List<double>();
+        }
+        public int x { get; set; }
+        public int y { get; set; }
+        public List<double> NeighboursDistances { get; set; }
+        public string ToString()
+        {
+            return x + "," + y;
+        }
+        public double GetDistance(Point neighbour)
+        {
+            return Math.Sqrt(Math.Pow((neighbour.x - x), 2) + Math.Pow((neighbour.y - y), 2));
+        }
+        public static bool operator == (Point a, Point b)
+        {
+            if (!(a is null) && !(b is null) && a.x == b.x && a.y == b.y)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Point a, Point b)
+        {
+            if ((a is null) && (b is null) && a.x == b.x && a.y == b.y)
+                return false;
+            else
+                return true;
+        }
+    }
+}
---
+++ ProblemSzpitali/ProblemSzpitali/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ProblemSzpitali
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var points = DataReader.GetPoints();
+
+            for (int i = 0; i < points.Count; i++) 
+            {
+                var neighbours = points[i].NeighboursDistances;
+                for (int j = 0; j < points.Count; j++) 
+                {
+                    var distance = points[i].GetDistance(points[j]);
+                    if (distance != 0)
+                    {
+                        neighbours.Add(distance);
+                    }
+                }
+            }
+
+            foreach (var point in points) 
+            {
+                Console.WriteLine("");
+                Console.WriteLine(point.ToString());
+                foreach (var distances in point.NeighboursDistances)
+                {
+                    Console.WriteLine(distances.ToString());
+                } 
+            }
+        }
+    }
+}
